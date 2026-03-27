0daf7ec15e560469b91ccc651a06e9ef3a6cffd0
JuliaKramek <julia.kramek13@gmail.com>
2026-03-27 18:15:29 +0100

    Add files via upload

---
+++ gold/create_tables.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE TABLE FLIGHTS.GOLD.TRAVEL_GOLD AS
+SELECT
+a.ticketing_airline,
+r.origin,
+r.destination,
+f.cabin,
+COUNT(*) AS total_transactions
+FROM FLIGHTS.SILVER.FACT_TRAVEL f
+JOIN FLIGHTS.SILVER.DIM_AIRLINE a
+ON f.airline_id = a.airline_id
+JOIN FLIGHTS.SILVER.DIM_ROUTE r
+ON f.route_id = r.route_id
+GROUP BY
+a.ticketing_airline,
+r.origin,
+r.destination,
+f.cabin;
