672f1450a06cc4a33a30b39f700ad8a0f7ae456f
JuliaKramek <julia.kramek13@gmail.com>
2026-03-27 19:37:10 +0200

    Update create_tables.sql

--- gold/create_tables.sql
+++ gold/create_tables.sql
@@ -6,9 +6,9 @@ r.destination,
 f.cabin,
 COUNT(*) AS total_transactions
 FROM FLIGHTS.SILVER.FACT_TRAVEL f
-JOIN FLIGHTS.SILVER.DIM_AIRLINE a
+JOIN FLIGHTS.SILVER.AIRLINE a
 ON f.airline_id = a.airline_id
-JOIN FLIGHTS.SILVER.DIM_ROUTE r
+JOIN FLIGHTS.SILVER.ROUTE r
 ON f.route_id = r.route_id
 GROUP BY
 a.ticketing_airline,
