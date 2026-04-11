0908fd325b9548e88c62945b10e7b9dd4136b5ec
JuliaKramek <julia.kramek13@gmail.com>
2026-04-11 14:13:33 +0200

    Update silver.py

--- orchestration/silver.py
+++ orchestration/silver.py
@@ -1,6 +1,16 @@
+from pyspark.sql import SparkSession
+
 def run_silver():
     print("Running SILVER layer")
 
+    spark = SparkSession.builder \
+        .appName("silver-layer") \
+        .getOrCreate()
+
+    print("Processing using Spark engine")
+
     print("Executing silver/load_silver.sql")
+
+    spark.stop()
 
     print("SILVER finished\n")
