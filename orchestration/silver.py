f7b5601a4500eec49ed63ebbb321cf988404f78f
JuliaKramek <julia.kramek13@gmail.com>
2026-04-20 23:16:11 +0200

    Delete orchestration directory

--- orchestration/silver.py
+++
@@ -1,9 +0,0 @@
-def run_silver():
-    print("Running SILVER layer")
-    print("Cleaning bronze data")
-    print("Removing nulls")
-    print("Deduplicating rows")
-    print("Executing silver/load_silver.sql")
-    print("SILVER finished\n")
-
-run_silver()
