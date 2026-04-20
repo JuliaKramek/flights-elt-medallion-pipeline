17e95a680ffc37ac2cc85000253999eff12e86ca
JuliaKramek <julia.kramek13@gmail.com>
2026-04-20 23:42:14 +0200

    Update flights_pipeline.py

--- airflow/dags/flights_pipeline.py
+++ airflow/dags/flights_pipeline.py
@@ -1,27 +1,36 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "Julia",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=2)
+}
 
 with DAG(
     dag_id="flights_pipeline",
     start_date=datetime(2026, 4, 20),
-    schedule_interval=None,
-    catchup=False
+    schedule_interval="@daily",
+    catchup=False,
+    default_args=default_args,
+    description="ELT Medallion pipeline for airline transactions"
 ) as dag:
 
     bronze = BashOperator(
-        task_id="bronze",
-        bash_command="python /opt/project/orchestration/bronze.py"
+        task_id="bronze_layer",
+        bash_command="python /opt/project/bronze/bronze.py"
     )
 
     silver = BashOperator(
-        task_id="silver",
+        task_id="silver_layer",
         bash_command="spark-submit /opt/project/silver/silver_job.py"
     )
 
     gold = BashOperator(
-        task_id="gold",
-        bash_command="python /opt/project/orchestration/gold.py"
+        task_id="gold_layer",
+        bash_command="python /opt/project/gold/gold.py"
     )
 
     bronze >> silver >> gold
