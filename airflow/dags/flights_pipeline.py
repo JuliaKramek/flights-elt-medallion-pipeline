1fe874d43c6eddad19ddc947ea9d24f866dd1484
JuliaKramek <julia.kramek13@gmail.com>
2026-04-21 16:11:44 +0200

    Update flights_pipeline.py

--- airflow/dags/flights_pipeline.py
+++ airflow/dags/flights_pipeline.py
@@ -6,8 +6,10 @@ default_args = {
     "owner": "Julia",
     "depends_on_past": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=1)
+    "retry_delay": timedelta(minutes=1),
 }
+
+PG = 'PGPASSWORD=airflow psql -h postgres -U airflow -d flights -f'
 
 with DAG(
     dag_id="flights_pipeline",
@@ -15,23 +17,57 @@ with DAG(
     schedule_interval="@daily",
     catchup=False,
     default_args=default_args,
-    description="ELT Medallion pipeline for airline transactions"
+    description="ELT Medallion Pipeline for airline transactions",
+    tags=["elt", "medallion", "postgres"],
 ) as dag:
 
-    bronze = BashOperator(
-        task_id="bronze_layer",
-        bash_command="python /opt/project/orchestration/bronze.py"
+    bronze_create_schema = BashOperator(
+        task_id="bronze_create_schema",
+        bash_command=f"{PG} /opt/project/bronze/create_schema.sql"
     )
 
-    silver = BashOperator(
-        task_id="silver_layer",
-        bash_command="python /opt/project/silver/silver_job.py"
-
+    bronze_create_tables = BashOperator(
+        task_id="bronze_create_tables",
+        bash_command=f"{PG} /opt/project/bronze/create_tables.sql"
     )
 
-    gold = BashOperator(
-        task_id="gold_layer",
-        bash_command="python /opt/project/orchestration/gold.py"
+    bronze_load_bronze = BashOperator(
+        task_id="bronze_load_bronze",
+        bash_command=f"{PG} /opt/project/bronze/load_bronze.sql"
     )
 
-    bronze >> silver >> gold
+    silver_create_schema = BashOperator(
+        task_id="silver_create_schema",
+        bash_command=f"{PG} /opt/project/silver/create_schema.sql"
+    )
+
+    silver_create_tables = BashOperator(
+        task_id="silver_create_tables",
+        bash_command=f"{PG} /opt/project/silver/create_tables.sql"
+    )
+
+    silver_load = BashOperator(
+        task_id="silver_load",
+        bash_command=f"{PG} /opt/project/silver/load_silver.sql"
+    )
+
+    gold_create_schema = BashOperator(
+        task_id="gold_create_schema",
+        bash_command=f"{PG} /opt/project/gold/create_schema.sql"
+    )
+
+    gold_create_tables = BashOperator(
+        task_id="gold_create_tables",
+        bash_command=f"{PG} /opt/project/gold/create_tables.sql"
+    )
+
+    (
+        bronze_create_schema
+        >> bronze_create_tables
+        >> bronze_load_bronze
+        >> silver_create_schema
+        >> silver_create_tables
+        >> silver_load
+        >> gold_create_schema
+        >> gold_create_tables
+    )
