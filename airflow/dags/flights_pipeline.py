0410291dfbdaba88e93dd133cd6c0fc3af00224a
JuliaKramek <julia.kramek13@gmail.com>
2026-04-25 18:30:24 +0200

    Update flights_pipeline.py

--- airflow/dags/flights_pipeline.py
+++ airflow/dags/flights_pipeline.py
@@ -9,11 +9,11 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
 }
 
-PG = "PGPASSWORD=airflow psql -h postgres -U airflow -d flights -f"
+PG = 'PGPASSWORD=airflow psql -h postgres -U airflow -d flights -f'
 
 with DAG(
     dag_id="flights_pipeline",
-    start_date=datetime(2025, 1, 1),
+    start_date=datetime(2026, 4, 20),
     schedule_interval="@daily",
     catchup=False,
     default_args=default_args,
@@ -21,55 +21,46 @@ with DAG(
     tags=["elt", "medallion", "postgres"],
 ) as dag:
 
-    # BRONZE
     bronze_create_schema = BashOperator(
         task_id="bronze_create_schema",
-        bash_command=f"{PG} /opt/project/bronze/create_schema.sql",
+        bash_command=f"{PG} /opt/project/bronze/create_schema.sql"
     )
 
     bronze_create_tables = BashOperator(
         task_id="bronze_create_tables",
-        bash_command=f"{PG} /opt/project/bronze/create_tables.sql",
+        bash_command=f"{PG} /opt/project/bronze/create_tables.sql"
     )
 
     bronze_load_bronze = BashOperator(
         task_id="bronze_load_bronze",
-        bash_command=f"{PG} /opt/project/bronze/load_bronze.sql",
+        bash_command=f"{PG} /opt/project/bronze/load_bronze.sql"
     )
 
-    # SILVER
     silver_create_schema = BashOperator(
         task_id="silver_create_schema",
-        bash_command=f"{PG} /opt/project/silver/create_schema.sql",
+        bash_command=f"{PG} /opt/project/silver/create_schema.sql"
     )
 
     silver_create_tables = BashOperator(
         task_id="silver_create_tables",
-        bash_command=f"{PG} /opt/project/silver/create_tables.sql",
+        bash_command=f"{PG} /opt/project/silver/create_tables.sql"
     )
 
     silver_load = BashOperator(
         task_id="silver_load",
-        bash_command=f"{PG} /opt/project/silver/load_silver.sql",
+        bash_command=f"{PG} /opt/project/silver/load_silver.sql"
     )
 
-    # GOLD
     gold_create_schema = BashOperator(
         task_id="gold_create_schema",
-        bash_command=f"{PG} /opt/project/gold/create_schema.sql",
+        bash_command=f"{PG} /opt/project/gold/create_schema.sql"
     )
 
     gold_create_tables = BashOperator(
         task_id="gold_create_tables",
-        bash_command=f"{PG} /opt/project/gold/create_tables.sql",
+        bash_command=f"{PG} /opt/project/gold/create_tables.sql"
     )
 
-    gold_load = BashOperator(
-        task_id="gold_load",
-        bash_command=f"{PG} /opt/project/gold/load_gold.sql",
-    )
-
-    # PIPELINE FLOW
     (
         bronze_create_schema
         >> bronze_create_tables
@@ -79,5 +70,4 @@ with DAG(
         >> silver_load
         >> gold_create_schema
         >> gold_create_tables
-        >> gold_load
     )
