0df627bb54967aa769ad9774f80a61ffda0d763b
JuliaKramek <julia.kramek13@gmail.com>
2026-04-25 19:43:23 +0200

    Update flights_pipeline.py

--- airflow/dags/flights_pipeline.py
+++ airflow/dags/flights_pipeline.py
@@ -13,14 +13,15 @@ PG = 'PGPASSWORD=airflow psql -h postgres -U airflow -d flights -f'
 
 with DAG(
     dag_id="flights_pipeline",
-    start_date=datetime(2026, 4, 20),
-    schedule_interval="@daily",
+    start_date=datetime(2024, 1, 1),  # 🔥 poprawione
+    schedule_interval=None,           # 🔥 manual trigger (lepsze na demo)
     catchup=False,
     default_args=default_args,
-    description="ELT Medallion Pipeline for airline transactions",
-    tags=["elt", "medallion", "postgres"],
+    description="ELT Medallion Pipeline with Kafka streaming",
+    tags=["elt", "medallion", "kafka"],
 ) as dag:
 
+    # --- BRONZE ---
     bronze_create_schema = BashOperator(
         task_id="bronze_create_schema",
         bash_command=f"{PG} /opt/project/bronze/create_schema.sql"
@@ -36,6 +37,7 @@ with DAG(
         bash_command=f"{PG} /opt/project/bronze/load_bronze.sql"
     )
 
+    # --- SILVER ---
     silver_create_schema = BashOperator(
         task_id="silver_create_schema",
         bash_command=f"{PG} /opt/project/silver/create_schema.sql"
@@ -51,6 +53,7 @@ with DAG(
         bash_command=f"{PG} /opt/project/silver/load_silver.sql"
     )
 
+    # --- GOLD ---
     gold_create_schema = BashOperator(
         task_id="gold_create_schema",
         bash_command=f"{PG} /opt/project/gold/create_schema.sql"
@@ -61,6 +64,13 @@ with DAG(
         bash_command=f"{PG} /opt/project/gold/create_tables.sql"
     )
 
+    # --- STREAMING (Kafka Consumer) ---
+    start_streaming = BashOperator(
+        task_id="start_streaming",
+        bash_command="python /opt/project/streaming/consumer.py"
+    )
+
+    # --- PIPELINE FLOW ---
     (
         bronze_create_schema
         >> bronze_create_tables
@@ -70,4 +80,5 @@ with DAG(
         >> silver_load
         >> gold_create_schema
         >> gold_create_tables
+        >> start_streaming   
     )
