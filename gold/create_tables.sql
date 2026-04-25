d61c7f2fa1c1a9dc977037fdc5654d00b0991055
JuliaKramek <julia.kramek13@gmail.com>
2026-04-25 18:23:45 +0200

    Update create_tables.sql

--- gold/create_tables.sql
+++ gold/create_tables.sql
@@ -1,49 +1,19 @@
-CREATE TABLE IF NOT EXISTS FLIGHTS.GOLD.TRAVEL_GOLD (
-    ticketing_airline VARCHAR,
-    origin VARCHAR,
-    destination VARCHAR,
-    cabin VARCHAR,
-    total_transactions NUMBER
-);
+DROP TABLE IF EXISTS gold.travel_gold;
 
-MERGE INTO FLIGHTS.GOLD.TRAVEL_GOLD t
-USING (
-    SELECT
-        a.ticketing_airline,
-        r.origin,
-        r.destination,
-        f.cabin,
-        COUNT(*) AS total_transactions
-    FROM FLIGHTS.SILVER.FACT_TRAVEL f
-    JOIN FLIGHTS.SILVER.AIRLINE a
-        ON f.airline_id = a.airline_id
-    JOIN FLIGHTS.SILVER.ROUTE r
-        ON f.route_id = r.route_id
-    GROUP BY
-        a.ticketing_airline,
-        r.origin,
-        r.destination,
-        f.cabin
-) s
-ON t.ticketing_airline = s.ticketing_airline
-AND t.origin = s.origin
-AND t.destination = s.destination
-AND t.cabin = s.cabin
-
-WHEN MATCHED THEN UPDATE SET
-    total_transactions = s.total_transactions
-
-WHEN NOT MATCHED THEN INSERT (
-    ticketing_airline,
-    origin,
-    destination,
-    cabin,
-    total_transactions
-)
-VALUES (
-    s.ticketing_airline,
-    s.origin,
-    s.destination,
-    s.cabin,
-    s.total_transactions
-);
+CREATE TABLE gold.travel_gold AS
+SELECT
+    a.ticketing_airline,
+    r.origin,
+    r.destination,
+    f.cabin,
+    COUNT(*) AS total_transactions
+FROM silver.fact_travel f
+JOIN silver.dim_airline a
+    ON f.airline_id = a.airline_id
+JOIN silver.dim_route r
+    ON f.route_id = r.route_id
+GROUP BY
+    a.ticketing_airline,
+    r.origin,
+    r.destination,
+    f.cabin;
